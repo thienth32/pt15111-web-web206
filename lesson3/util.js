d18680dbc8537e635f8815b5edee36dd667916e2
thienth32 <thienth32@gmail.com>
2020-07-17 13:02:21 +0700

    update lesson 3

---
+++ lesson3/util.js
@@ -0,0 +1,8 @@
+var System = {
+    logUsername: function(){
+        document.write('Hello: FPT Poly');
+    },
+    logout: function(username){
+        document.write(`Tài khoản ${username} đã thoát`);
+    }
+}
