ecc81eeb6692912724f38e4fa6f3f266d2eda3d7
thienth32 <thienth32@gmail.com>
2020-07-24 13:54:16 +0700

    update

---
+++ lesson6/public/js/main.js
@@ -0,0 +1,43 @@
+window.SystemCore = {
+    baseApiUrl: 'https://5f1a7522610bde0016fd292f.mockapi.io/hotels',
+    fetchData: function(){
+        fetch(this.baseApiUrl)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            if(data.length > 0){
+                let content = ``;
+                data.map(function(item, index){
+                    content += `<tr id="data-${item.id}">
+                                    <td>${item.id}</td>
+                                    <td>${item.name}</td>
+                                    <td>
+                                        <img src="${item.logo}" width="50"/>
+                                    </td>
+                                    <td>${item.address}</td>
+                                    <td>
+                                        <button type="button" 
+                                            class="btn btn-danger btn-sm"
+                                            onclick="SystemCore.removeHotel(${item.id})">Xóa</button>
+                                    </td>
+                                </tr>`;                
+                });
+
+                document.querySelector('tbody').innerHTML = content;
+            }
+        })
+    },
+    removeHotel: function(hotelId){
+        // gửi request để xóa dữ liệu từ mockapi
+        let removeUrl = `${this.baseApiUrl}/${hotelId}`;
+        fetch(removeUrl, {method: "DELETE"})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // xóa dom trên trình duyệt
+            document.querySelector(`#data-${data.id}`).remove();
+        });
+    }
+}
